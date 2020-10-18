3659b4c66d5360f741410bb9e14e9b9c53f037ff
Slaboym1e <57869517+Slaboym1e@users.noreply.github.com>
2020-10-18 21:21:03 +0700

    add composition of matrix

--- CJ_LM/main.cpp
+++ CJ_LM/main.cpp
@@ -3,23 +3,13 @@
 #include "main.h"
 int main()
 {
-	matrix m1(3, 3, 0);
-	int counter = 1;
-	for (int i = 0; i < 3; i++)
-		for (int j = 0; j < 3; j++)
-		{
-			m1.Get_m(i, j) = counter;
-			counter++;
-		}
-	matrix m2(3, 3, 1);
-	//std::cout << m2;
-	matrix m3(3, 3, 0);
-	m3 = m1 - m2;
-	std::cout << m1;
-	std::cout << std::endl;
+	matrix m1(2, 2, 0);
+	matrix m2(2, 2, 0);
+	m1.Get_m(0, 0) = m2.Get_m(0, 0) = 1;
+	m1.Get_m(1, 0) = m2.Get_m(1, 0) = 2;
+	m1.Get_m(0, 1) = m2.Get_m(0, 1) = 5;
+	m1.Get_m(1, 1) = m2.Get_m(1, 1) = 6;
+	m2 = m1 * m2;
 	std::cout << m2;
-	std::cout << std::endl;
-	std::cout << m3;
-
 	return 0;
 }
--- TESTS/TESTS.cpp
+++ TESTS/TESTS.cpp
@@ -63,5 +63,19 @@ namespace TESTS
 					counter++;
 				}
 		}
+		TEST_METHOD(testcomp)
+		{
+			matrix m1(2, 2, 0);
+			matrix m2(2, 2, 0);
+			m1.Get_m(0, 0) = m2.Get_m(0, 0) = 1;
+			m1.Get_m(1, 0) = m2.Get_m(1, 0) = 2;
+			m1.Get_m(0, 1) = m2.Get_m(0, 1) = 5;
+			m1.Get_m(1, 1) = m2.Get_m(1, 1) = 6;
+			m2 = m1 * m2;
+			Assert::AreEqual(m2.Get_m(0, 0),11);
+			Assert::AreEqual(m2.Get_m(1, 0), 14);
+			Assert::AreEqual(m2.Get_m(0, 1), 35);
+			Assert::AreEqual(m2.Get_m(1, 1), 46);
+		}
 	};
 }
